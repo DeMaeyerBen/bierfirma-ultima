402d9860ee87873c77f29ef673a5f857a557b2b2
Sam De Maeyer <sam_demaeyer@hotmail.com>
2019-04-23 17:13:25 +0200

    Add project

---
+++ js/custom.js
@@ -0,0 +1,184 @@
+var mapData = [
+  {
+    description: 'Dranken Gijbels,<br> Schoebroekstraat 63, 3583 Paal',
+    coordinates: {
+      lat: 51.0607868,
+      lng: 5.1646182
+    }
+  },{
+    description: 'Ad Fundum,<br> Zepperenweg 7b, 3800 Sint-Truiden',
+    coordinates: {
+      lat: 50.8163039,
+      lng: 5.1932346
+    }
+  },{
+    description: 'De Weyer Cafe,<br> Oud-Mersenhoven 15, 3720 Kortessem',
+    coordinates: {
+      lat: 50.858525,
+      lng: 5.3898143
+    }
+  },{
+    description: 'SK Herkenrode,<br> Hentjeslaan 3, 3500 Hasselt',
+    coordinates: {
+      lat: 50.9428091,
+      lng: 5.2946529
+    }
+  },{
+    description: 'Bierpunt Genk,<br> Zuivelmarkt 29, 3500 Hasselt',
+    coordinates: {
+      lat: 50.9636822,
+      lng: 5.4965319
+    }
+  },{
+    description: 'Bierpunt Diepenbeek,<br> Marktplein, 3590 Diepenbeek',
+    coordinates: {
+      lat: 50.9075316,
+      lng: 5.4180699
+    }
+  },{
+    description: 'Bierpunt Hasselt,<br> Zuivelmarkt 29, 3500 Hasselt',
+    coordinates: {
+      lat: 50.9314709,
+      lng: 5.338127
+    }
+  },{
+    description: '\'t Bierhuis,<br> Pieter Vanhoudtstraat 64a, 3582 Beringen',
+    coordinates: {
+      lat: 51.0555287,
+      lng: 5.2732084
+    }
+  },{
+    description: 'De Kring,<br> Guido Gezellelaan 13, 3550 Heusden-Zolder',
+    coordinates: {
+      lat: 51.0295472,
+      lng: 5.2765802
+    }
+  },{
+    description: 'Carrefour market Diepenbeek,<br> Wijkstraat 37, 3590 Diepenbeek',
+    coordinates: {
+      lat: 50.9075294,
+      lng: 5.425208
+    }
+  },{
+    description: 'Koestal,<br> Kolonel Dusartplein 42, 3500 Hasselt',
+    coordinates: {
+      lat: 50.9320803,
+      lng: 5.3424565
+    }
+  }, {
+    description: 'Corda Bar,<br> Kempische Steenweg 311, 3500 Hasselt',
+    coordinates: {
+      lat: 50.9525128,
+      lng: 5.3498204
+    }
+  }, {
+    description: 'Depot 30,<br> Hasseltweg 363, 3600 Genk',
+    coordinates: {
+      lat: 50.9577579,
+      lng: 5.4343551
+    }
+  }, {
+    description: 'Het Hemelrijk,<br> Hemelrijk 11, 3500 Hasselt ',
+    coordinates: {
+      lat: 50.930083,
+      lng: 5.339783
+    }
+  }, {
+    description: 'De Cambrinus,<br> Dorpstraat 25, 3500 Hasselt',
+    coordinates: {
+      lat: 50.932234,
+      lng: 5.334754
+    }
+  }, {
+    description: 'Sportpunt Runkst,<br> Djef Swennenstraat 17, 3500 Hasselt',
+    coordinates: {
+      lat: 50.926514,
+      lng: 5.319018
+    }
+  }, {
+    description: 'Rugbyclub Hasselt,<br> Sint-Janshoefstraat z/n, 3511 Kuringen',
+    coordinates: {
+      lat: 50.952363,
+      lng: 5.332323
+    }
+  }, {
+    description: 'Bowling Epsilon,<br> Wijkstraat 39, 3590 Diepenbeek',
+    coordinates: {
+      lat: 50.907339,
+      lng: 5.428882
+    }
+  }, {
+    description: 'Cafe \'t Zant,<br> Halveweg 28, 3520 Zonhoven',
+    coordinates: {
+      lat: 50.992454,
+      lng: 5.344344
+    }
+  }, {
+    description: 'Marlou dranken Prik&tik,<br> Heikensstraat 11, 3520 Zonhoven',
+    coordinates: {
+      lat: 50.994703,
+      lng: 5.338271
+    }
+  }, {
+    description: 'Dranken Houben,<br> Rechterstraat 69, 3511 Kuringen',
+    coordinates: {
+      lat: 50.950854,
+      lng: 5.336802
+    }
+  }, {
+    description: 'Het Pleintje,<br> Sint-Hubertusplein 19, 3500 Hasselt',
+    coordinates: {
+      lat: 50.924994,
+      lng: 5.325815
+    }
+  }, {
+    description: 'Sanlet,<br> Torenstraat 35, 3500 Hasselt',
+    coordinates: {
+      lat: 50.924749,
+      lng: 5.328913
+    }
+  }, {
+    description: 'Kan10 SYNTRA Limburg campus Hasselt,<br> 3500 Hasselt',
+    coordinates: {
+      lat: 50.937185,
+      lng: 5.360710
+    }
+  }, {
+    description: 'Bowling Anglo,<br> Kleinhulst 6, 9220 Hamme',
+    coordinates: {
+      lat: 51.097693,
+      lng: 4.140037
+    }
+  }, {
+    description: 'Quo Vadis,<br> Dorpsstraat 46, 3520 Zonhoven',
+    coordinates: {
+      lat: 50.993055,
+      lng: 5.368186
+    }
+  }, 
+];
+
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    scrollwheel: false,
+    center: { lat: 50.9279681, lng: 5.337154 }
+  });
+  var infowindows = [];
+  mapData.forEach(function(data) {
+    var marker = new google.maps.Marker({
+      position: data.coordinates,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: data.description
+    });
+    var infowindow = new google.maps.InfoWindow({
+      content: '<h3>' + data.description + '</h3>'
+    });
+    infowindows.push(infowindow);
+    marker.addListener('click', function() {
+      infowindows.forEach(function(iw) { iw.close(); });
+      infowindow.open(map, marker);
+    });
+  });
+}
