11f0af1d2103751e72be4b72d2a555d70a4be6c6
DeMaeyerBen <47723569+DeMaeyerBen@users.noreply.github.com>
2020-04-18 13:16:10 +0200

    Locations (#1)
    * De Zonhoeve Sporthal
    
    * commits
    
    * last commit
    
    * latest changes
    
    * Translations
    
    * Add french

---
+++ js/translations/en.js
@@ -0,0 +1,22 @@
+const translationsEn = {
+  mainTitle: 'Drink, Experience and Enjoy!',
+  orderOnline: 'Order Online',
+  find: 'Find Ultima',
+  aboutUs: 'About Us',
+  dryHoppedText: 'This atypical Belgian triple contains American hops and orange peel which are responsible for the freshness of the beer. This triple is recommended to drink on hot summer days. It has a pleasant bitterness that keeps resonating on the tongue. The beer contains 8% alcohol and is available in 33cl bottles.',
+  dateDryHopped: 'Anno February 2019',
+  beeHoneyText: 'A blond beer with addition of Belgian honey from the beekeeping <a href ="https://www.thehappybees.be/" target="_blank" rel="noopener noreferrer">The Happy Bees</a> located in Hasselt. The beer contains the sweetness from the honey and light herbal aromatics. It has an alcohol percentage of 7% and is available in 33cl bottles.',
+  dateBeeHoney: 'Anno December 2019',
+  rumOakedText: 'This Barley Wine with 10% of alcohol is a dark beer that has matured on oak chips, soaked in Belgian Rum Double aged from <a href ="https://www.ghostinabottle.be/" target="_blank" rel="noopener noreferrer">Ghost in a Bottle</a>. A unique palette of flavours including chocolate, vanilla, wood and many more. This whole-craft beer is brewed as a limited edition of 640 bottles. They’re only available in 75cl bottles.',
+  dateRumOaked: 'Anno December 2019',
+  tryUltima: 'Try Ultima today!',
+  introUltima: 'Ultima already available in your area? If there are missing outlets, don\'t hesitate and contact us!',
+  findUltima: 'Find Ultima!',
+  contactUs: 'Contact Us!',
+  pubs: 'Pubs',
+  shops: 'Shops',
+  aboutUsText: 'We are 4 friends with a passion for brewing and recipe development. Out of that passion, we currently created 3 beers: a blond honey beer with an alcohol percentage of 7%, an Exotic Triple with 8% of alcohol and lastly a Barley Wine containing 10% of alcohol. With these beers we want to leave our mark on the large beer market in Belgium. Owning a brewery may still be a long way off, but hopefully somewhere in the future.',
+  beerLover: 'BeerLover',
+  zythologist: 'Zythologist',
+  zythologistBrewer: 'Zythologist <br>Brewer'
+}
